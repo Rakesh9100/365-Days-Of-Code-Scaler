105a73e4895cf9ed8273e3a325e0b98ae2529f12
Rakesh Roshan <roshanrakesh7362@gmail.com>
2024-01-02 23:45:17 +0530

    Day 1

---
+++ Day1.cpp
@@ -0,0 +1,16 @@
+// https://www.interviewbit.com/problems/product-of-digits/
+
+int Solution::solve(int A) {
+    
+    if(A == 0) {
+        return 0 ;
+    }
+    int prod = 1 ;
+    while (A) {
+        int digit = A % 10 ;
+        prod *= digit ;
+        A /= 10 ;
+    }
+    return prod ;
+    
+}
