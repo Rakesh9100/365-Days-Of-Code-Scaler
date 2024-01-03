6b0c26aea54e0afd9bd982e9317b128ca7cba12b
Rakesh Roshan <roshanrakesh7362@gmail.com>
2024-01-03 23:51:13 +0530

    Day 2

---
+++ Day2.cpp
@@ -0,0 +1,15 @@
+// https://www.interviewbit.com/problems/hello-world/
+
+#include<iostream>
+
+using namespace std;
+
+int main()  {
+    // YOUR CODE GOES HERE
+    // Please take input and print output to standard input/output (stdin/stdout)
+    // E.g. 'cin' for input & 'cout' for output
+    
+    cout<< "Hello, InterviewBit!"<<endl;
+    
+    return 0;
+}
