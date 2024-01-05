20c590a4b6f6788543ee67284a22d134de078b0a
Rakesh Roshan <roshanrakesh7362@gmail.com>
2024-01-05 23:38:44 +0530

    Day 4

---
+++ Day4.cpp
@@ -0,0 +1,16 @@
+// https://www.interviewbit.com/problems/type-conversion/
+
+#include<iostream>
+
+using namespace std;
+
+int main()  {
+    char ch;
+    cin>>ch;
+    //Your code goes here
+    
+    int a;
+    a = int(ch);
+    cout << a << endl;
+    return 0;
+}
