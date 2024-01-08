1f2c7ebc87987e17d63c72b93f6ecce71d882a1d
Rakesh Roshan <roshanrakesh7362@gmail.com>
2024-01-08 23:44:57 +0530

    Day 7

---
+++ Day07.cpp
@@ -0,0 +1,11 @@
+// https://www.interviewbit.com/problems/random-numbers/
+
+Answer the question below:
+
+What will be the output of the following code?
+
+int main(){
+    cout<< rand()%1 <<endl;
+}
+
+Answer --> 0
