ce0b63fb378444f8a968d2445bc5db6ffed1c4dc
Rakesh Roshan <roshanrakesh7362@gmail.com>
2024-01-10 23:43:10 +0530

    Day 9

---
+++ Day09.cpp
@@ -0,0 +1,44 @@
+// https://www.interviewbit.com/problems/switch-statement/
+
+#include<iostream>
+
+using namespace std;
+
+int main()  {
+    int weekday;
+    cin>>weekday;
+    // YOUR CODE GOES HERE
+    
+    switch (weekday) {
+    case 1:
+        cout << "Monday";
+        break;
+       
+    case 2:
+        cout << "Tuesday";
+        break;
+       
+    case 3:
+        cout << "Wednesday";
+        break;
+       
+    case 4:
+        cout << "Thursday";
+        break;
+       
+    case 5:
+        cout << "Friday";
+        break;
+       
+    case 6:
+        cout << "Saturday";
+        break;
+       
+    case 7:
+        cout << "Sunday";
+        break;    
+    }
+    
+    return 0;
+    
+}
