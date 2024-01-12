a961080a567cc9612a2ed27222d7ad80d2cd6dcc
Rakesh Roshan <roshanrakesh7362@gmail.com>
2024-01-12 23:22:53 +0530

    Day 11

---
+++ Day11.cpp
@@ -0,0 +1,23 @@
+// Loops
+
+// https://www.interviewbit.com/problems/loops/
+
+#include<iostream>
+
+using namespace std;
+
+int main()  {
+    int N;
+    cin>>N;
+    // YOUR CODE GOES HERE
+    
+    for(int i = 0; i <= N; i++)
+    {
+        if(i % 2 != 0)
+        {
+            cout << i << endl;
+        }
+    }
+    
+    return 0;
+}
