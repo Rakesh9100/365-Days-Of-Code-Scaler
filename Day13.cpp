3c59be1e764dcd6eb5c7df3cef77be1ad15f1502
Rakesh Roshan <roshanrakesh7362@gmail.com>
2024-01-14 23:35:15 +0530

    Day 13

---
+++ Day13.cpp
@@ -0,0 +1,23 @@
+// Functions - Basic
+
+// https://www.interviewbit.com/problems/functions-basic/
+
+/*
+#include<iostream>
+using namespace std;
+*/
+
+// Your Code goes here
+
+int compute(int A, int B) {
+    return A * A + B * B;
+}
+
+/*
+int main()  {
+    int A, B;
+    cin>>A>>B;
+    cout<<compute(A, B)<<endl;
+    return 0;
+}
+*/
