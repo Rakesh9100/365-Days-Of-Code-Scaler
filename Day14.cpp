145cea7aa4e9250b97597197dfca7d12d0f8e808
Rakesh Roshan <roshanrakesh7362@gmail.com>
2024-01-15 15:56:50 +0530

    Day 14

---
+++ Day14.cpp
@@ -0,0 +1,28 @@
+// Passing parameters
+
+// https://www.interviewbit.com/problems/passing-parameters/
+
+/*
+#include<iostream>
+using namespace std;
+*/
+
+// Your code goes here
+
+void compute(int& A, int& B, int& C) {
+    A = A * A * A;
+    B = B * B * B;
+    C = C * C * C;
+}
+
+/*
+int main()  {
+    int A, B, C;
+    cin>>A>>B>>C;
+    compute(A, B, C);
+    cout<<A<<endl;
+    cout<<B<<endl;
+    cout<<C<<endl;
+    return 0;
+}
+*/
