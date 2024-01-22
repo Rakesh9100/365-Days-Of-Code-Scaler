0af552233b2e27b2982223e296bd5ed6c008421a
Rakesh Roshan <roshanrakesh7362@gmail.com>
2024-01-22 20:26:07 +0530

    Day 21

---
+++ Day21.cpp
@@ -0,0 +1,34 @@
+// Structures
+
+// https://www.interviewbit.com/problems/structures/
+
+/*
+#include<iostream>
+#include<string>
+using namespace std;
+*/
+
+
+// Create Structure named 'student'
+
+int main()  {
+    // Your code goes here
+    
+    struct student {
+        string name;
+        int rollno;
+        int   marks;      
+    };
+    
+    student s1;
+    
+    s1.name = "Robin";
+    s1.rollno = 11;
+    s1.marks = 86;
+    
+    // Don't change the below code
+    cout<<s1.name<<endl;
+    cout<<s1.rollno<<endl;
+    cout<<s1.marks<<endl;
+    return 0;
+}
