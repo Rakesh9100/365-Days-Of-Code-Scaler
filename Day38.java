685a5081f8d844c6fcc42ce6431a92c42c4385b3
Rakesh Roshan <roshanrakesh7362@gmail.com>
2024-02-08 19:59:39 +0530

    Day 38

---
+++ Day38.java
@@ -0,0 +1,27 @@
+// Datatypes
+
+// https://www.interviewbit.com/problems/datatypes/
+
+import java.lang.*;
+import java.util.*;
+
+public class Day38 {
+    public static void main(String[] args) {
+        /*
+        Assign values to the following variable as described above
+        Don't change the variable name
+        */
+        String mystring1 = "InterviewBit";
+        String mystring2 = "Don't change the variable name";
+        int myint = 11;
+        boolean myboolean = true;
+        
+        
+        /*Don't change the code below*/
+        System.out.println("String1 = " + mystring1);
+        System.out.println("String2 = " + mystring2);
+        System.out.println("int = " + myint);
+        System.out.println("boolean = " + myboolean);
+        
+    }
+}
