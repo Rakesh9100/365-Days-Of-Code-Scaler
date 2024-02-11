733c29af06221b4d7a866f8bf5ed8cbceb9ed576
Rakesh Roshan <roshanrakesh7362@gmail.com>
2024-02-11 23:07:53 +0530

    Day 41

---
+++ Day41.java
@@ -0,0 +1,23 @@
+// Loops
+
+// https://www.interviewbit.com/problems/loops-java/
+
+import java.lang.*;
+import java.util.*;
+
+public class Day41 {
+    public static void main(String[] args) {
+        /***Don't change the code here***/
+        Scanner inp = new Scanner(System.in);
+        int n = inp.nextInt();
+        inp.close();
+        /***********************************/
+        
+        /***Your code goes here***/
+        
+        for(int i = 0; i < n; i++) {
+            System.out.println(i + 1);
+        }
+        
+    }
+}
