96163ff60898b5132845bdf38c5f50508a9d22df
Rakesh Roshan <roshanrakesh7362@gmail.com>
2024-02-14 22:20:32 +0530

    Day 44

---
+++ Day44.java
@@ -0,0 +1,36 @@
+// MultiDimensional Arrays
+
+// https://www.interviewbit.com/problems/multidimensional-arrays/
+
+import java.lang.*;
+import java.util.*;
+
+public class Day44 {
+    public static void main(String[] args) {
+        // YOUR CODE GOES HERE
+        // Please take input and print output to standard input/output (stdin/stdout)
+        // DO NOT USE ARGUMENTS FOR INPUTS
+        // E.g. 'Scanner' for input & 'System.out' for output
+        
+        Scanner scan = new Scanner(System.in);
+        int m = scan.nextInt();
+        int n = scan.nextInt();
+        
+        int[][] arr = new int[m][n];
+       
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                arr[i][j] = scan.nextInt();
+            }
+        }
+       
+        for(int i = 0; i < n; i++) {
+            int sum = 0;
+            for(int j = 0; j < m; j++) {
+                sum = sum + arr[j][i];
+            }
+            System.out.print(sum + " ");
+        }
+
+    }
+}
