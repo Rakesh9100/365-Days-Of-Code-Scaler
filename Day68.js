267c24e195b1050830a13b4509f41531e89a4a31
Rakesh Roshan <roshanrakesh7362@gmail.com>
2024-03-09 22:29:17 +0530

    Day 68

---
+++ Day68.js
@@ -0,0 +1,10 @@
+// Input and Output
+
+// https://www.interviewbit.com/problems/input-and-output-js/
+
+// Please take input and print output to standard input/output (stdin/stdout)
+// DO NOT USE ARGUMENTS FOR INPUTS
+// YOUR CODE GOES HERE
+
+var a = readLine()
+console.log("Hello", a)
