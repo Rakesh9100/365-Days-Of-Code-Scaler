0b291d05a13f64e40d39b1e9e9dfe9b937b93471
Rakesh Roshan <roshanrakesh7362@gmail.com>
2024-03-10 19:21:00 +0530

    Day 69

---
+++ Day69.js
@@ -0,0 +1,11 @@
+// Datatypes And Type Casting
+
+// https://www.interviewbit.com/problems/datatypes-and-type-casting/
+
+// Please take input and print output to standard input/output (stdin/stdout)
+// DO NOT USE ARGUMENTS FOR INPUTS
+// YOUR CODE GOES HERE
+
+let a = readLine();
+let p = Number(a);
+console.log(p + 1);
