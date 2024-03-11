553e99e310b410e490131c3099a053b3606460e8
Rakesh Roshan <roshanrakesh7362@gmail.com>
2024-03-11 22:54:35 +0530

    Day 70

---
+++ Day70.js
@@ -0,0 +1,27 @@
+// Comparision Operation & If-Else
+
+// https://www.interviewbit.com/problems/js-comparision-operation-if-else/
+
+// DO NOT USE ARGUMENTS FOR INPUTS
+// Input num valus is in variable *num*
+var num = readLine().trim();
+// YOUR CODE GOES HERE
+
+if (num >= 90) {
+    console.log("A")
+}
+else if (num >= 80) {
+    console.log("B")
+}
+else if (num >= 70) {
+    console.log("C")
+}
+else if (num >= 60) {
+    console.log("D")
+}
+else if (num >= 50) {
+    console.log("E")
+}
+else {
+    console.log("F")
+}
