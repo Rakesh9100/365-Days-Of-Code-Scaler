2614666aa7d3261f7390dcb493e03a255822d0d6
Rakesh Roshan <roshanrakesh7362@gmail.com>
2024-03-12 22:47:03 +0530

    Day 71

---
+++ Day71.js
@@ -0,0 +1,35 @@
+// Switch Statement
+
+// https://www.interviewbit.com/problems/js-switch-statement/
+
+// DO NOT USE ARGUMENTS FOR INPUTS
+// Input num valus is in variable *num*
+var num = parseInt(readLine().trim());
+// YOUR CODE GOES HERE
+
+switch(num) {
+    case 1 :
+        console.log("Monday");
+        break;
+    case 2 :
+        console.log("Tuesday");
+        break;
+    case 3 :
+        console.log("Wednesday");
+        break;
+    case 4 :
+        console.log("Thursday");
+        break;
+    case 5 :
+        console.log("Friday");
+        break;
+    case 6 :
+        console.log("Saturday");
+        break;
+    case 7 :
+        console.log("Sunday");
+        break;
+    default :
+        console.log("Choice other then 1, 2, 3, 4, 5, 6 and 7");
+        break;
+}
