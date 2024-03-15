4c49c3bf2de2c3b1d0ec20e68a9c3d993bfdbda6
Rakesh Roshan <roshanrakesh7362@gmail.com>
2024-03-15 19:19:05 +0530

    Day 74

---
+++ Day74.js
@@ -0,0 +1,11 @@
+// Arithmetic Operations on Numbers
+
+// https://www.interviewbit.com/problems/arithmetic-operations-on-numbers/
+
+// DO NOT USE ARGUMENTS FOR INPUTS
+// Input num valus is in variable *num*
+var num = parseInt(readLine().trim());
+// YOUR CODE GOES HERE
+
+var x = (num * num) + (num ** 3) - num;
+console.log(x)
