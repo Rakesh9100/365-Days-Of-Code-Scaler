ab3d7a60f2dd1b01660a4fd4a265710733250a1c
Rakesh Roshan <roshanrakesh7362@gmail.com>
2024-03-17 11:28:24 +0530

    Day 76

---
+++ Day76.js
@@ -0,0 +1,9 @@
+// Bits and operations on it
+
+// https://www.interviewbit.com/problems/bits-and-operations-on-it/
+
+// DO NOT USE ARGUMENTS FOR INPUTS
+// Input num valus is in variable *num*
+var num = parseInt(readLine().trim());
+// YOUR CODE GOES HERE
+console.log(~num);
