ab26b5fd38fe651f5f4c27c7849b2b0360f372b4
Rakesh Roshan <roshanrakesh7362@gmail.com>
2024-03-18 19:15:27 +0530

    Day 77

---
+++ Day77.js
@@ -0,0 +1,11 @@
+// JavaScript Strings
+
+// https://www.interviewbit.com/problems/javascript-strings/
+
+// DO NOT USE ARGUMENTS FOR INPUTS
+// Input num value is in variable *num*
+var num = readLine().trim();
+// YOUR CODE GOES HERE
+var x = num.length;
+var y = num.concat(x);
+console.log(y);
