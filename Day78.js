ad1746c744b4178f58dd032d5062fc99d382e90e
Rakesh Roshan <roshanrakesh7362@gmail.com>
2024-03-19 22:29:28 +0530

    Day 78

---
+++ Day78.js
@@ -0,0 +1,10 @@
+// String Methods
+
+// https://www.interviewbit.com/problems/string-methods/
+
+// DO NOT USE ARGUMENTS FOR INPUTS
+// Input num value is in variable *num*
+var num = readLine().trim();
+// YOUR CODE GOES HERE
+var a = num.toUpperCase()
+console.log(num + a)
