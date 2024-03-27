7ea1a91cf89d3c0161994b87c1e9c0779c738dbf
Rakesh Roshan <roshanrakesh7362@gmail.com>
2024-03-27 16:57:03 +0530

    Day 86

---
+++ Day86.js
@@ -0,0 +1,20 @@
+// Classes And Their Instances
+
+// https://www.interviewbit.com/problems/classes-and-their-instances/
+
+// Please take input and print output to standard input/output (stdin/stdout)
+// DO NOT USE ARGUMENTS FOR INPUTS
+
+var defaultName = readLine();  
+var defaultPosition = readLine();
+var defaultSkill = readLine();
+
+class Employee{
+    
+    // define the constructor function here
+    constructor() {
+        this.name = defaultName;
+        this.position = defaultPosition;
+        this.skill = defaultSkill;
+    }
+}
